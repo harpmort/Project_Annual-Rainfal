0873f245b2d4f9b5a0bf80e6635fb37f5600ba26
TheNior <s8070049@kmitl.ac.th>
2015-12-03 17:05:43 +0700

    Update main.py
    Update data add Annual-Rainfall2014

--- main.py
+++ main.py
@@ -31,3 +31,19 @@ def fetch_data_2012():
     else:
       info2013[table2013[time][3]] += sum(rain)
   return info2013
+
+def fetch_data_2014():
+  """bring all data in files to dictionary and sum values of each dictionary"""
+  rainfall2014_part1=open('RainfallHourlyData2014_Part1.csv',newline='')
+  rainfall2014_part2=open('RainfallHourlyData2014_Part2.csv',newline='')
+  data1 = csv.reader(rainfall2014_part1)
+  data2 = csv.reader(rainfall2014_part2)
+  table2014=[row for row in data1]+[row for row in data2]
+  info2014 = {}
+  for time in range(len(table2014)):
+    rain = [float(i) for i in table2014[time][-23:]]
+    if table2014[time][3] not in info2014:
+      info2014[table2014[time][3]] = sum(rain)
+    else:
+      info2014[table2014[time][3]] += sum(rain)
+  return info2014
